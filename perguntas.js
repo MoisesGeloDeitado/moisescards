9c589c021a3e8cdaded0d34a5b9f7d57a46baf04
MoisesGeloDeitado <00001107740939sp@al.educacao.sp.gov.br>
2024-11-27 14:42:23 -0300

    Flashcard

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Programação',
+    'Como é programar em Python?',
+    'Isso significa que é uma linguagem que funciona para criar uma grande variedade de aplicações diferentes.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que afeta o meio ambiente?',
+    'A intervenção do homem tem acelerado a degradação da natureza. Desmatamentos, poluição de rios, mares e florestas, e o uso indevido da água e de energia são alguns exemplos.'
+)
+
+criaCartao(
+    'Programação',
+    'Como é a vida de quem trabalha com programação?',
+    'Sua função é criar, projetar e implementar soluções digitais, englobando desde aplicativos e sites até sistemas de gerenciamento de dados ou softwares.'
+)
+
+criaCartao(
+    'Quimica',
+    'o que sao cadeias fechadas ou cíclicas?',
+    'As cadeias cíclicas subdividem-se em dois grupos: cadeias aromáticas e cadeias alicíclicas, ou não-aromáticas, ou cicloalifáticas.'
+)
+
+criaCartao(
+    'Educação fisica',
+    'Qual a importância do esporte na Educação Física?',
+    'O esporte é uma ferramenta de socialização, autoconhecimento e consciência de coletividade que influencia diretamente na formação de caráter dos indivíduos.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Você é um estudante em inglês?',
+    'Você é um estudante em Inglês é You are a student.'
+)
+
+criaCartao(
+    'Fisíca',
+    'O que é carbono 14 e para que serve?',
+    'O carbono-14 é um isótopo radioativo de número de massa 14 que é assimilado por todos os organismos vegetais e animais e, por isso, é usado na datação de fósseis.'
+)
+
+criaCartao(
+    'matematica',
+    'Quais são as 3 áreas da Matemática?',
+    'Álgebra: estuda a manipulação de incógnitas, inseridas em equações e em outras formas algébricas. Trigonometria: estuda as funções trigonométricas e investiga as relações entre as medidas angulares de triângulos. Geometria: é o estudo das dimensões espaciais de figuras geométricas, tais como área e volume.'
+)
